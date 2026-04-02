3db4586f68bdefe11f27f6189bb5954783b91fcc
hrostohar8 <hrostohar@tvz.hr>
2026-04-02 15:45:13 +0200

    Delete lab-1/TicketingSystemFightNight/debug.py
    Delete debug

--- lab-1/TicketingSystemFightNight/debug.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open('chat.json', 'r', encoding='utf-8') as f:
-    data = json.load(f)
-
-# Pogledaj CIJELI prvi request
-print("=== KOMPLETAN PRVI REQUEST ===")
-print(json.dumps(data['requests'][0], indent=2))
